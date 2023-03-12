5d5d51ccc0a2c23059a3ef4506f3e0a2fe3f7b7e
Yo7X <117323614+Yo7X@users.noreply.github.com>
2023-03-12 12:15:32 -0700

    Update main.js

--- Identity V tools/main.js
+++ Identity V tools/main.js
@@ -20,6 +20,8 @@ function bt1() {
     document.getElementById('pg004').style.display = 'none'
     document.getElementById('bt005').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('pg005').style.display = 'none'
+    document.getElementById('bt006').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg006').style.display = 'none'
 
     document.getElementById('default').style.display = 'none'
 }
@@ -36,6 +38,8 @@ function bt2() {
     document.getElementById('pg004').style.display = 'none'
     document.getElementById('bt005').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('pg005').style.display = 'none'
+    document.getElementById('bt006').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg006').style.display = 'none'
 
     document.getElementById('default').style.display = 'none'
 }
@@ -52,6 +56,8 @@ function bt3() {
     document.getElementById('pg004').style.display = 'none'
     document.getElementById('bt005').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('pg005').style.display = 'none'
+    document.getElementById('bt006').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg006').style.display = 'none'
 
     document.getElementById('default').style.display = 'none'
 }
@@ -68,6 +74,8 @@ function bt4() {
     document.getElementById('pg003').style.display = 'none'
     document.getElementById('bt005').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('pg005').style.display = 'none'
+    document.getElementById('bt006').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg006').style.display = 'none'
 
     document.getElementById('default').style.display = 'none'
 }
@@ -84,6 +92,26 @@ function bt5() {
     document.getElementById('pg003').style.display = 'none'
     document.getElementById('bt004').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
     document.getElementById('pg004').style.display = 'none'
+    document.getElementById('bt006').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg006').style.display = 'none'
+
+    document.getElementById('default').style.display = 'none'
+}
+
+function bt6() {
+    document.getElementById('bt006').style.backgroundColor = 'rgba(40, 26, 192, 0.582)'
+    document.getElementById('pg006').style.display = 'block'
+
+    document.getElementById('bt001').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg001').style.display = 'none'
+    document.getElementById('bt002').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg002').style.display = 'none'
+    document.getElementById('bt003').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg003').style.display = 'none'
+    document.getElementById('bt004').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg004').style.display = 'none'
+    document.getElementById('bt005').style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
+    document.getElementById('pg005').style.display = 'none'
 
     document.getElementById('default').style.display = 'none'
 }
