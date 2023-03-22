d6d6f7c144669658f1d120090653ffd99012964c
Yo7X <117323614+Yo7X@users.noreply.github.com>
2023-03-22 11:52:35 -0700

    Update bingo.js

--- Identity V tools/bingo.js
+++ Identity V tools/bingo.js
@@ -1,42 +1,60 @@
-var myArray = [];
-var randomNumbers = [];
-var ids = 1;
-
-function scramble(){
-
-    randomNumbers = [];
-    ids = 1;
-    cut = 0;
-
-    myArray = [
-        `Toxic postmatch`,
-        `A tier lobby`,
-        `Default skin`,
-        `Sketchy username`,
-        `A badge`,
-        `B badge`,
-        `C badge`,
-        `Awful cipher pop`,
-        `Missed flywheel`,
-        `Terror shock`,
-        `Sit full chair time`,
-        `Self promo`,
-        `Hunter recovery cancel`,
-        `SS skin`,
-        `No borrowed time`,
-        `rescuer with no tide`,
-        `Free space`
+spaces = [
+    `Toxic postmatch`,
+    `A tier lobby`,
+    `Default skin`,
+    `Sketchy username`,
+    `A badge`,
+    `B badge`,
+    `C badge`,
+    `Awful cipher pop`,
+    `Missed flywheel`,
+    `Terror shock`,
+    `Sit full chair time`,
+    `Self promo`,
+    `Hunter recovery cancel`,
+    `SS skin`,
+    `No borrowed time`,
+    `rescuer with no tide`,
+    `Free space`
 ];
 
-for (var i = 0; i < 17; i++) {
-    var randomIndex = Math.floor(Math.random() * myArray.length);
-    randomNumbers.push(myArray[randomIndex]);
-    myArray.splice(randomIndex, 1);
+temp = [];
 
-    x = 'card_' + ids
-    document.getElementById(x).style.backgroundColor = 'rgba(20, 14, 82, 0.582)'
-    document.getElementById(x).innerHTML = randomNumbers[cut]
-    ids++
-    cut++
+function scramble(){
+    id = 1
+
+    temp = spaces.slice();
+
+    for (let i = 0; i < 17; i++) {
+        x = Math.floor(Math.random() * temp.length)
+        document.getElementById('card_' + id).innerHTML = temp[x]
+        temp.splice(x, 1)
+        id++
+    }
 }
+
+function addspace() {
+    x = document.getElementById('typespace').value
+    if (x == '') {
+        alert('You need to put something in the text box')
+    } else {
+        spaces.push(x)
+        document.getElementById('typespace').value = ''
+    }
+}
+
+function removespace() {
+    x = document.getElementById('typespace').value
+
+    if (x == '') {
+        alert('You need to put something in the text box\nRemember its case sensitive')
+    } else {
+        for (let i = 0; i < spaces.length; i++) {
+            if (spaces[i] == x) {
+                spaces.splice(i, 1)
+                document.getElementById('typespace').value = ''
+                break;
+            }
+        }
+    }
 }
